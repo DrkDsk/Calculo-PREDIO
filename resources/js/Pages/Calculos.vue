f773894f12fcce29a7f0921a734e966104db2366
Alfredo Palacios <joseaph.1998@gmail.com>
2023-07-29 00:26:34 -0600

    instalación de vue

---
+++ resources/js/Pages/Calculos.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { Head } from '@inertiajs/vue3';
+
+</script>
+
+<template>
+    <Head title="Dashboard" />
+    <h1>hello world</h1>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CalculosController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('/calculos', CalculosController::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
