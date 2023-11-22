a673c81737435c6fd2c09a62257f78d829b8e11b
Alfredo Palacios <alfredopalacios@blue.gen.in>
2023-11-22 14:09:24 -0600

    variable hecha computed

--- resources/js/Pages/Calculos/Create.vue
+++ resources/js/Pages/Calculos/Create.vue
@@ -8,11 +8,6 @@ const props = defineProps({
     ground : Object
 })
 
-const inpcActual    = ref(0)
-const inpcDePago    = ref(0)
-const importe       = ref(0)
-const tasaRecargo   = ref(0)
-
 const meses = [
     "Enero",
     "Febrero",
@@ -38,47 +33,39 @@ const formCalculation = useForm({
 })
 
 const recargo = computed(() => {
-  let inpcActual  = parseFloat(formCalculation.inpcActual)
-  let inpcDePago  = parseFloat(formCalculation.inpcDePago)
-  let importe     = round(parseFloat(formCalculation.importe))
-  let tasaRecargo = parseFloat(formCalculation.tasaDeRecargo)
-  let actualizacionPlusImporte = (importe * (inpcActual / inpcDePago))
+  let tasaRecargo = formCalculation.tasaDeRecargo
 
-  if (!importe || !inpcActual || !inpcDePago || !tasaRecargo) {
+  if (!tasaRecargo) {
     return null;
   }
 
-  return round(actualizacionPlusImporte * tasaRecargo)
+  return round(actualizacionPlusImporte() * tasaRecargo)
 })
 
-const actualizacionTotal = computed(() => {
-  let inpcActual  = parseFloat(formCalculation.inpcActual)
-  let inpcDePago  = parseFloat(formCalculation.inpcDePago)
+const actualizacionPlusImporte = () => {
+  let inpcActual  = formCalculation.inpcActual
+  let inpcDePago  = formCalculation.inpcDePago
   let importe     = round(formCalculation.importe)
 
   if (!importe || !inpcActual || !inpcDePago) {
     return null;
   }
 
-  let actualizacionPlusImporte = (importe * (inpcActual / inpcDePago))
-  return round(actualizacionPlusImporte - importe)
+  return (importe * (inpcActual / inpcDePago))
+}
+
+const actualizacion = computed(() => {
+  return round(actualizacionPlusImporte() - round(formCalculation.importe))
 })
 
 const importarTotal = computed(() => {
-    let inpcActual  = parseFloat(formCalculation.inpcActual)
-    let inpcDePago  = parseFloat(formCalculation.inpcDePago)
-    let importe     = round(parseFloat(formCalculation.importe))
-    let tasaRecargo = parseFloat(formCalculation.tasaDeRecargo)
-    let actualizacionPlusImporte = (importe * (inpcActual / inpcDePago))
-    let actualizacion          = round(actualizacionPlusImporte - importe)
+  let importe = round(formCalculation.importe)
 
-    if (!importe || !inpcActual || !inpcDePago || !tasaRecargo) {
-      return null;
-    }
+  if (!importe || !actualizacion.value || !recargo.value) {
+    return null;
+  }
 
-    let recargo                = round(actualizacionPlusImporte * tasaRecargo)
-
-    return round(importe + actualizacion + recargo)
+    return round(importe + actualizacion.value + recargo.value)
 })
 
 const round = (number) => {
@@ -92,17 +79,17 @@ const round = (number) => {
 }
 
 const saveOperation = () => {
-    router.post(route('terrenos.calculos.store', props.ground.id), {
+  router.post(route('terrenos.calculos.store', props.ground.id), {
         'ground_id'               : props.ground.id,
-        'amount'                  : importe.value,
-        'updated_charge'          : actualizacionTotal,
-        'surcharge'               : recargo,
-        'amount_to_pay'           : importarTotal,
+        'amount'                  : formCalculation.importe,
+        'updated_charge'          : actualizacion.value,
+        'surcharge'               : recargo.value,
+        'amount_to_pay'           : importarTotal.value,
         'month_at_operation_date' : mesActual,
         'month_of_pay'            : meses[mesSeleccionado.value-1],
-        'INCP_at_operation_date'  : inpcActual.value,
-        'INCP_applied'            : inpcDePago.value,
-        'surcharge_rate'          : tasaRecargo.value
+        'INCP_at_operation_date'  : formCalculation.inpcActual,
+        'INCP_applied'            : formCalculation.inpcDePago,
+        'surcharge_rate'          : formCalculation.tasaDeRecargo
     })
 }
 </script>
@@ -148,16 +135,13 @@ const saveOperation = () => {
                             <label class="absolute px-2 ml-2 -mt-3 font-medium text-gray-600 bg-white">Tasa de Recargo</label>
                             <input v-model="formCalculation.tasaDeRecargo" type="text" class="block w-full px-4 py-4 mt-2 text-base placeholder-gray-400 bg-white border border-gray-300 rounded-md focus:outline-none focus:border-black" placeholder="Ingresa la tasa de recargo">
                         </div>
-                        <div class="relative">
-                            <button class="inline-block w-full px-5 py-4 text-xl font-medium text-center text-white transition duration-200 bg-blue-600 rounded-lg hover:bg-blue-500 ease">Calcular</button>
-                        </div>
                     </form>
                 </div>
             </div>
             <div v-if="importarTotal" class="ml-8 z-10 flex flex-col items-start justify-start p-10 bg-white shadow-2xl rounded-xl">
                 <h4 class="w-full text-4xl font-medium leading-snug">Resultados:</h4>
                 <h3 class="w-full text-xl font-medium leading-snug">Importe: <span class="text-yellow-600">{{ round(formCalculation.importe) }}</span></h3>
-                <h3 class="w-full text-xl font-medium leading-snug">Actualización: <span class="text-blue-600">{{actualizacionTotal}}</span></h3>
+                <h3 class="w-full text-xl font-medium leading-snug">Actualización: <span class="text-blue-600">{{actualizacion}}</span></h3>
                 <h3 class="w-full text-xl font-medium leading-snug">Recargo: <span class="text-red-600">{{recargo}}</span></h3>
                 <div class="linea-divisoria">
                     <h3 class="w-full text-xl font-medium leading-snug">Importe a Pagar: <span class="text-green-600">{{importarTotal}}</span></h3>
