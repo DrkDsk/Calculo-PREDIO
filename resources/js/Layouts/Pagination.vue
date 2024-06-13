6876c08077d919c254a9347d5ddb6f4dad9c102c
Alfredo Palacios <joseaph.1998@gmail.com>
2024-06-12 23:36:05 -0600

    implementacion de xlsx para mostrar excel en vista de calculos

--- resources/js/Layouts/Pagination.vue
+++ resources/js/Layouts/Pagination.vue
@@ -9,8 +9,8 @@ defineProps({
     <div>
         <div class="flex flex-wrap -mb-1">
             <template v-for="(link, p) in links" :key="p">
-                <div v-if="link.url === null" class="mr-1 mb-1 px-4 py-3 text-sm leading-4 text-gray-400 border rounded" v-html="link.label" />
-                <Link v-else class="mr-1 mb-1 px-4 py-3 text-sm leading-4 border rounded hover:bg-white hover:text-blue-700 focus:border-indigo-500 focus:text-indigo-500" :class="{ 'bg-blue-700 text-white': link.active }" :href="link.url" v-html="link.label"></Link>
+                <div v-if="link.url === null" class="mr-1 mb-1 px-4 py-3 text-sm leading-4 text-white border rounded" v-html="link.label" />
+                <Link v-else class="text-white mr-1 mb-1 px-4 py-3 text-sm leading-4 border rounded hover:bg-white hover:text-blue-700 focus:border-indigo-500 focus:text-indigo-500" :class="{ 'bg-blue-700 text-white': link.active }" :href="link.url" v-html="link.label"></Link>
             </template>
         </div>
     </div>
