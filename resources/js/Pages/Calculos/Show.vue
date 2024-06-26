758157d59bcea38de9a57311b4d7f740831c0647
Alfredo Palacios <joseaph.1998@gmail.com>
2024-06-26 12:26:30 -0600

    refactor de modelos

--- resources/js/Pages/Calculos/Show.vue
+++ resources/js/Pages/Calculos/Show.vue
@@ -22,8 +22,8 @@ const round = (number) => {
 <template>
     <Navbar>
         <div class="w-11/12 mx-auto mt-10">
-            <div class="flex flex-col items-start justify-center w-full lg:flex-row">
-                <div class="relative z-10 w-full">
+            <div class="flex flex-col items-start justify-center w-full lg:flex-row space-x-3">
+                <div class="relative z-10 w-9/12">
                     <div class="relative z-10 flex flex-col items-start justify-start p-10 bg-white shadow-2xl rounded-xl">
                         <h4 class="text-4xl font-medium leading-snug text-sky-700">Cálculo</h4>
                         <form class="relative mt-6 space-y-8 w-full">
@@ -33,7 +33,7 @@ const round = (number) => {
                                         <label class="absolute px-2 ml-2 -mt-3 bg-gray-100 rounded-md font-medium text-gray-600">
                                             Año del cálculo
                                         </label>
-                                        <input disabled v-model="balance.year_at_operation_date" type="text"
+                                        <input disabled readonly  v-model="balance.year_at_operation_date" type="text"
                                                class="bg-gray-100 block w-full px-4 py-4 mt-2 text-base placeholder-gray-400 border border-gray-300 rounded-md focus:outline-none focus:border-black">
                                     </div>
 
@@ -41,7 +41,7 @@ const round = (number) => {
                                         <label class="absolute px-2 ml-2 -mt-3 bg-gray-100 rounded-md font-medium text-gray-600">
                                             Año en que se debió pagar
                                         </label>
-                                        <input disabled v-model="balance.due_payment_year" type="text"
+                                        <input disabled readonly  v-model="balance.due_payment_year" type="text"
                                                class="bg-gray-100 block w-full px-4 py-4 mt-2 text-base placeholder-gray-400 border border-gray-300 rounded-md focus:outline-none focus:border-black">
                                     </div>
 
@@ -49,7 +49,7 @@ const round = (number) => {
                                         <label class="absolute px-2 ml-2 -mt-3 bg-gray-100 rounded-md font-medium text-gray-600">
                                             Mes del cálculo
                                         </label>
-                                        <input disabled v-model="balance.month_at_operation_date" type="text"
+                                        <input disabled readonly v-model="balance.month_at_operation_date" type="text"
                                                class="bg-gray-100 block w-full px-4 py-4 mt-2 text-base placeholder-gray-400 border border-gray-300 rounded-md focus:outline-none focus:border-black">
                                     </div>
 
@@ -65,7 +65,7 @@ const round = (number) => {
                                         <label class="rounded-md absolute px-2 ml-2 -mt-3 font-medium text-gray-600">
                                             INPC Actual (Mes anterior al actual)
                                         </label>
-                                        <input v-model="balance.INCP_at_operation_date" type="text"
+                                        <input disabled readonly  v-model="balance.INCP_at_operation_date" type="text"
                                                class="block w-full px-4 py-4 mt-2 text-base placeholder-gray-400 border border-gray-300 rounded-md focus:outline-none focus:border-black"
                                                placeholder="Ingresa la cantidad del INPC">
                                     </div>
@@ -73,14 +73,14 @@ const round = (number) => {
                                     <div class="my-4">
                                         <label class="rounded-md absolute px-2 ml-2 -mt-3 font-medium text-gray-600">INPC Correspondiente
                                             (Mes que se debió pagar)</label>
-                                        <input v-model="balance.INCP_applied" type="text"
+                                        <input disabled readonly v-model="balance.INCP_applied" type="text"
                                                class="block w-full px-4 py-4 mt-2 text-base placeholder-gray-400 border border-gray-300 rounded-md focus:outline-none focus:border-black"
                                                placeholder="Ingresa la cantidad del INPC Correspondiente">
                                     </div>
 
                                     <div class="relative">
                                         <label class="rounded-md absolute px-2 ml-2 -mt-3 font-medium text-gray-600">Importe</label>
-                                        <input v-model="balance.amount" type="text"
+                                        <input disabled readonly v-model="balance.amount" type="text"
                                                class="block w-full px-4 py-4 mt-2 text-base placeholder-gray-400 border border-gray-300 rounded-md focus:outline-none focus:border-black"
                                                placeholder="Ingresa el importe">
                                     </div>
@@ -88,7 +88,7 @@ const round = (number) => {
                                     <div class="relative">
                                         <label class="rounded-md absolute px-2 ml-2 -mt-3 font-medium text-gray-600">Tasa de
                                             Recargo</label>
-                                        <input v-model="balance.surcharge_rate" type="text"
+                                        <input disabled readonly v-model="balance.surcharge_rate" type="text"
                                                class="block w-full px-4 py-4 mt-2 text-base placeholder-gray-400 border border-gray-300 rounded-md focus:outline-none focus:border-black"
                                                placeholder="Ingresa la tasa de recargo">
                                     </div>
@@ -97,7 +97,7 @@ const round = (number) => {
                         </form>
                     </div>
                 </div>
-                <div class="ml-8 z-10 flex flex-col items-start justify-start p-10 bg-white shadow-2xl rounded-xl">
+                <div class="z-10 flex flex-col items-start justify-start p-10 bg-white shadow-2xl rounded-xl">
                     <h4 class="w-full text-4xl font-medium leading-snug">Resultados:</h4>
                     <h3 class="w-full text-xl font-medium leading-snug">Importe: <span
                         class="text-yellow-600">${{round(balance.amount)}}</span></h3>
