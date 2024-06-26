19e07828aa4baa980b10887e3a69a7b50941f7a3
Alfredo Palacios <joseaph.1998@gmail.com>
2024-06-26 14:05:05 -0600

    refactor de modelos

--- resources/js/Components/BalanceReportCardItem.vue
+++ resources/js/Components/BalanceReportCardItem.vue
@@ -8,12 +8,12 @@ defineProps({
 </script>
 
 <template>
-    <div class="max-w-sm mx-auto">
-        <div class="bg-white shadow-lg rounded-lg p-6 space-y-4 h-40">
+    <div class="mx-auto">
+        <div class="bg-white shadow-lg rounded-lg p-6 space-y-4 h-48">
             <div class="flex items-center">
                 <div class="space-y-4">
-                    <div class="text-gray-600 text-xl">{{label}}</div>
-                    <div class="text-gray-900 text-2xl font-semibold">
+                    <div class="text-gray-600 text-3xl font-black">{{label}}</div>
+                    <div class="text-2xl font-semibold text-green-600">
                         ${{value}}
                     </div>
                 </div>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -74,7 +74,7 @@ const saveOwner = () => {
                     <div class="w-full overflow-x-auto">
                         <table class="w-full">
                             <thead>
-                            <tr class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+                            <tr class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:0 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
                                 <th class="px-4 py-3">Propietario</th>
                                 <th class="px-4 py-3">Número Telefónico</th>
                                 <th class="px-4 py-3">RFC</th>
