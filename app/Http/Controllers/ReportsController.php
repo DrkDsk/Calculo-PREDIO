4465bc28b5df3a4ee8261abfa1744843e0883d89
DrkDsk <joseaph.1998@gmail.com>
2024-06-29 11:32:45 -0600

    reporte-actualizado

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -23,21 +23,21 @@ class ReportsController extends Controller
                   'label' => 'Total de importe a pagar:',
                   'value' => round(
                       Balance::where('due_payment_year', $duePaymentYear)
-                          ->where('month_at_operation_date', $monthAtOperationDate)
+                          ->where('month_of_pay', $monthAtOperationDate)
                           ->sum('amount_to_pay'),2),
                 ],
                 [
                     'label' => 'Total de recargos:',
                     'value' => round(
                         Balance::where('due_payment_year', $duePaymentYear)
-                            ->where('month_at_operation_date', $monthAtOperationDate)
+                            ->where('month_of_pay', $monthAtOperationDate)
                             ->sum('surcharge'),2)
                 ],
                 [
                     'label' => 'Total de actualizaciones:',
                     'value' => round(
                         Balance::where('due_payment_year', $duePaymentYear)
-                            ->where('month_at_operation_date', $monthAtOperationDate)
+                            ->where('month_of_pay', $monthAtOperationDate)
                             ->sum('updated_charge'),2)
                 ]
             ];
